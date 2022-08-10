44859726bc4ae5ea0d2e8cd3971587a72e0c89d7
ronanlevesque <ronan.levesque@gmail.com>
2022-08-10 12:00:20 +0200

    chore: update deps

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "@algolia/ui-library/public/css/fragments.css";
+import "./App.css";
 
 function App() {
   return <div className="App">Add your content here</div>;
